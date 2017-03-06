927146b16cf4f8176f1052cfdefbdf48955d51ee
kl1986 <kelvin.g.lee@gmail.com>
2017-03-06 17:07:04 +0000

    Shopping list mysql db implemented

--- app/src/main/java/com/example/a2287517l/mydrawerattempt/MainActivity.java
+++ app/src/main/java/com/example/a2287517l/mydrawerattempt/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
